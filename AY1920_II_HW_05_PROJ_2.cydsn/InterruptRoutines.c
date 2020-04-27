2c1878e1806249c2fbb6e91ff341332a0cb3e2e7
noemi <noemi.gozzi@mail.polimi.it>
2020-04-27 18:28:52 +0200

    Changed Bridge COntrol files in project 2, due to issues in old version. gitignore files added to each subfloder. PROJECT 2 CHECKED AND COMPLETED

--- AY1920_II_HW_05_PROJ_2.cydsn/InterruptRoutines.c
+++ AY1920_II_HW_05_PROJ_2.cydsn/InterruptRoutines.c
@@ -4,7 +4,8 @@ ISR FILE: it has been chosen to use an ISR on timer to deal with acceleration da
 Other methods (such us exploiting FIFO functionality and buffer) are less robust when dealing with real
 time application. Checking, with an ISR if new data are available (status register bit XYZDA=1), with a 
 frequency higher than the one of the system (i.e. f LIS3DH 100 HZ, ISR 400 Hz) ensures that data are not 
-missed (flag will be high not every ISR but only when status register confirm new data).
+missed; then inside ISR status register is checked, and the flag is high only when new data are available
+(not each ISR).
 -   This has been confirmed also in Bridge control Panel where the rate at which data are recevied and 
     plotted is around 98-102 data per seconds. (as per requirements)
 -   Furthermore it has been checked using cool-term and an error message that the frequency is enough and data
@@ -24,6 +25,7 @@ and data reading);
 #define LIS3DH_OUT_X_L 0x28
 #define LIS3DH_STATUS_REG 0x27
 #define STATUS_REGISTER_NEW_DATA 0x08 //status register bit[3]=1 when new data is available
+#define packet_dimension 6
 uint8_t status_register_newdata;
 ErrorCode error;
 
@@ -37,7 +39,7 @@ CY_ISR(Custom_ISR_Timer_XYZ){
     //when new data is available the read XYZ values with ReadRegisterMulti function.   
     if ((error==NO_ERROR)&&((status_register_newdata) & (STATUS_REGISTER_NEW_DATA))){
         
-            error=I2C_Peripheral_ReadRegisterMulti(LIS3DH_DEVICE_ADDRESS, LIS3DH_OUT_X_L, 6 , (&Acceleration[0]));
+            error=I2C_Peripheral_ReadRegisterMulti(LIS3DH_DEVICE_ADDRESS, LIS3DH_OUT_X_L, packet_dimension , (&Acceleration[0]));
             //if data are correctly read, then communicate to main that data are ready to be sent.
             if(error == NO_ERROR){
             PacketReadyFlag=1;
